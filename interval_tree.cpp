7fcd68d4d992bf3a1799c3b8812a1d229ed02d1f
Manukrishna J P <themanu110206@gmail.com>
2026-01-29 23:23:40 +0530

    Added preorder printing also modified insert function in interval_tree.cpp

--- interval_tree.cpp
+++ interval_tree.cpp
@@ -16,20 +16,6 @@ struct Node{
 class IntervalTree{
 private:
     Node* root;
-public:
-    IntervalTree(int l,int h){
-        root=createNode(l,h);
-    }
-
-    Node* createNode(int low,int high){
-        Node* newnode=new Node();
-        newnode->i.low=low;
-        newnode->i.high=high;
-        newnode->right=NULL;
-        newnode->left=NULL;
-        newnode->max=high;
-        return newnode;
-    }
 
     Node* insert(Node* node,int l,int h){
         if(node==NULL){
@@ -46,11 +32,44 @@ public:
         return node;
     }
 
+    void preorder(Node* node){
+        if(node==NULL){
+            return;
+        }
+        cout<<"["<<node->i.low<<" , "<<node->i.high<<"]"<<", Max : "<<node->max<<" ";
+        preorder(node->left);
+        preorder(node->right);
+    }
+
+public:
+    IntervalTree(int l,int h){
+        root=createNode(l,h);
+    }
+
+    Node* createNode(int low,int high){
+        Node* newnode=new Node();
+        newnode->i.low=low;
+        newnode->i.high=high;
+        newnode->right=NULL;
+        newnode->left=NULL;
+        newnode->max=high;
+        return newnode;
+    }
+
+    void insert(int l,int h){
+        root=insert(root,l,h);
+    }
+
+    void preorder(){
+        preorder(root);
+    }
 };
 
-
-
-
 int main(){
-
+    IntervalTree it(15,20);
+    it.insert(10,30);
+    it.insert(17,19);
+    it.insert(5,20);
+    it.insert(12,15);
+    it.preorder();
 }
